f910f0844ac81f22138bd6fef4c0a83439f6142a
karl-zschiebsch <karl.zschiebsch@github.com>
2023-10-07 17:36:31 +0200

    Swap config from properties to ini

--- src/main/java/org/seekers/grpc/SeekersProperties.java
+++ src/main/java/org/seekers/grpc/SeekersProperties.java
@@ -1,22 +1,22 @@
 package org.seekers.grpc;
 
-import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 import java.util.function.Function;
 
 import javax.annotation.Nonnull;
 
 import io.scvis.proto.Corresponding;
+import org.ini4j.Ini;
 
 /**
  * The SeekerProperties class represents the properties for the Seeker game.
  */
 public class SeekersProperties implements Corresponding<Map<String, String>> {
 	@Nonnull
-	private static SeekersProperties defaultProperties = new SeekersProperties("server.properties");
+	private static SeekersProperties defaultProperties = new SeekersProperties("server.ini");
 
 	/**
 	 * Sets the default SeekerProperties instance.
@@ -38,7 +38,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	}
 
 	@Nonnull
-	private final Properties properties = new Properties();
+	private final Ini ini;
 
 	/**
 	 * Creates a SeekerProperties object and loads properties from the specified
@@ -47,9 +47,9 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @param pathname The path of the properties file.
 	 */
 	public SeekersProperties(String pathname) {
-		try (FileInputStream file = new FileInputStream(pathname)) {
-			properties.load(file);
-		} catch (IOException e) {
+		try {
+			this.ini = new Ini(new File(pathname));
+		}	catch (IOException e) {
 			throw new SeekersException(e);
 		}
 	}
@@ -61,31 +61,30 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * applies the conversion function to the corresponding value. If the key is not
 	 * found in the properties, it returns the default value.
 	 * 
-	 * @param <T>   The type of the value to be returned.
-	 * @param key   The key whose associated value is to be retrieved.
-	 * @param func  The conversion function to convert the property value to the
-	 *              desired type.
-	 * @param value The default value to be returned if the key is not found in the
-	 *              properties.
+	 * @param <T>     The type of the value to be returned.
+	 * @param section The section of the <code>ini</code> file.
+	 * @param option  The option of the section.
+	 * @param func    The conversion function to convert the property value to the
+	 *                desired type.
+	 * @param value   The default value to be returned if the key is not found in the
+	 *                properties.
 	 * @return The value associated with the key, or the default value if the key is
 	 *         not found.
 	 */
-	@SuppressWarnings("unchecked")
-	private <T> T getOrDefault(String key, Function<String, T> func, T value) {
-		return (T) used.computeIfAbsent(key,
-				k -> properties.containsKey(key) ? func.apply(properties.getProperty(key)) : value);
+	private <T> T getOrDefault(String section, String option, Function<String, T> func, T value) {
+		String row = ini.fetch(section, option);
+		return row == null ? value : func.apply(row);
 	}
 
-	@Nonnull
-	private final Map<String, Object> used = new HashMap<>();
-
 	public String getProjectExecCommand() {
-		return getOrDefault("project.exec-command", k -> k, "python3 run_clients.py");
+		return getOrDefault("project", "exec-command", k -> k, "python3 run_clients.py");
 	}
 
 	public String getProjectPathToAis() {
-		return getOrDefault("project.path-to-ais", k -> k, "ais/");
+		return getOrDefault("project", "path-to-ais", k -> k, "ais/");
 	}
+
+	private static final String GLOBAL = "global";
 
 	/**
 	 * Retrieves the value of the <code>"global.auto-play"</code> property.
@@ -93,7 +92,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"global.auto-play"</code> property.
 	 */
 	public boolean getGlobalAutoPlay() {
-		return getOrDefault("global.auto-play", Boolean::valueOf, false);
+		return getOrDefault(GLOBAL, "auto-play", Boolean::valueOf, false);
 	}
 
 	/**
@@ -102,7 +101,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"global.playtime"</code> property.
 	 */
 	public long getGlobalPlaytime() {
-		return getOrDefault("global.playtime", Long::valueOf, 50_000L);
+		return getOrDefault(GLOBAL, "playtime", Long::valueOf, 50_000L);
 	}
 
 	/**
@@ -111,7 +110,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"global.speed"</code> property.
 	 */
 	public double getGlobalSpeed() {
-		return getOrDefault("global.speed", Double::valueOf, 1.0);
+		return getOrDefault(GLOBAL, "speed", Double::valueOf, 1.0);
 	}
 
 	/**
@@ -120,7 +119,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"global.players"</code> property.
 	 */
 	public int getGlobalPlayers() {
-		return getOrDefault("global.players", Integer::valueOf, 2);
+		return getOrDefault(GLOBAL, "players", Integer::valueOf, 2);
 	}
 
 	/**
@@ -129,7 +128,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"global.seekers"</code> property.
 	 */
 	public int getGlobalSeekers() {
-		return getOrDefault("global.seekers", Integer::valueOf, 5);
+		return getOrDefault(GLOBAL, "seekers", Integer::valueOf, 5);
 	}
 
 	/**
@@ -138,7 +137,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"global.goals"</code> property.
 	 */
 	public int getGlobalGoals() {
-		return getOrDefault("global.goals", Integer::valueOf, 5);
+		return getOrDefault(GLOBAL, "goals", Integer::valueOf, 5);
 	}
 
 	/**
@@ -147,7 +146,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"map.width"</code> property.
 	 */
 	public int getMapWidth() {
-		return getOrDefault("map.width", Integer::valueOf, 768);
+		return getOrDefault("map", "width", Integer::valueOf, 768);
 	}
 
 	/**
@@ -156,7 +155,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"map.height"</code> property.
 	 */
 	public int getMapHeight() {
-		return getOrDefault("map.height", Integer::valueOf, 768);
+		return getOrDefault("map", "height", Integer::valueOf, 768);
 	}
 
 	/**
@@ -165,7 +164,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"camp.width"</code> property.
 	 */
 	public double getCampWidth() {
-		return getOrDefault("camp.width", Double::valueOf, 55.0);
+		return getOrDefault("camp", "width", Double::valueOf, 55.0);
 	}
 
 	/**
@@ -174,7 +173,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"camp.height"</code> property.
 	 */
 	public double getCampHeight() {
-		return getOrDefault("camp.height", Double::valueOf, 55.0);
+		return getOrDefault("camp", "height", Double::valueOf, 55.0);
 	}
 
 	/**
@@ -183,7 +182,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"physical.friction"</code> property.
 	 */
 	public double getPhysicalFriction() {
-		return getOrDefault("physical.friction", Double::valueOf, 0.1);
+		return getOrDefault("physical", "friction", Double::valueOf, 0.1);
 	}
 
 	/**
@@ -192,7 +191,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"seeker.magnet-slowdown"</code> property.
 	 */
 	public double getSeekerMagnetSlowdown() {
-		return getOrDefault("seeker.magnet-slowdown", Double::valueOf, 0.2);
+		return getOrDefault("seeker", "magnet-slowdown", Double::valueOf, 0.2);
 	}
 
 	/**
@@ -201,7 +200,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"seeker.disabled-time"</code> property.
 	 */
 	public double getSeekerDisabledTime() {
-		return getOrDefault("seeker.disabled-time", Double::valueOf, 250.0);
+		return getOrDefault("seeker", "disabled-time", Double::valueOf, 250.0);
 	}
 
 	/**
@@ -210,7 +209,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"seeker.radius"</code> property.
 	 */
 	public double getSeekerRadius() {
-		return getOrDefault("seeker.radius", Double::valueOf, 10.0);
+		return getOrDefault("seeker", "radius", Double::valueOf, 10.0);
 	}
 
 	/**
@@ -219,7 +218,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"seeker.mass"</code> property.
 	 */
 	public double getSeekerMass() {
-		return getOrDefault("seeker.mass", Double::valueOf, 1.0);
+		return getOrDefault("seeker", "mass", Double::valueOf, 1.0);
 	}
 
 	/**
@@ -228,7 +227,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"seeker.thrust"</code> property.
 	 */
 	public double getSeekerThrust() {
-		return getOrDefault("seeker.thrust", Double::valueOf, 0.1);
+		return getOrDefault("seeker", "thrust", Double::valueOf, 0.1);
 	}
 
 	/**
@@ -237,7 +236,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"goal.scoring-time"</code> property.
 	 */
 	public double getGoalScoringTime() {
-		return getOrDefault("goal.scoring-time", Double::valueOf, 100.0);
+		return getOrDefault("goal", "scoring-time", Double::valueOf, 100.0);
 	}
 
 	/**
@@ -246,7 +245,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"goal.radius"</code> property.
 	 */
 	public double getGoalRadius() {
-		return getOrDefault("goal.radius", Double::valueOf, 6.0);
+		return getOrDefault("goal", "radius", Double::valueOf, 6.0);
 	}
 
 	/**
@@ -255,7 +254,7 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"goal.mass"</code> property.
 	 */
 	public double getGoalMass() {
-		return getOrDefault("goal.mass", Double::valueOf, 0.5);
+		return getOrDefault("goal", "mass", Double::valueOf, 0.5);
 	}
 
 	/**
@@ -264,16 +263,19 @@ public class SeekersProperties implements Corresponding<Map<String, String>> {
 	 * @return The value of the <code>"goal.thrust"</code> property.
 	 */
 	public double getGoalThrust() {
-		return getOrDefault("goal.thrust", Double::valueOf, 0.1);
+		return getOrDefault("goal", "thrust", Double::valueOf, 0.1);
 	}
 
 	@Nonnull
 	@Override
 	public Map<String, String> associated() {
 		Map<String, String> build = new HashMap<>();
-		for (Map.Entry<String, Object> entry : used.entrySet()) {
-			build.put(entry.getKey(), entry.getValue().toString());
+		for (var section : ini.values()) {
+			for (var entry : section.entrySet()) {
+				build.put(section.getSimpleName() + "." + entry.getKey(), entry.getValue());
+			}
 		}
+		System.out.println(build);
 		return build;
 	}
 }
