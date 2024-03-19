4bcb76a913d67601c2df0cba0c0c97013d51b48f
Karl Zschiebsch <karl.zschiebsch@gmail.com>
2024-03-19 18:33:04 +0100

    Add plugins and argparser, removed default python support

---
+++ src/main/java/org/seekers/plugin/LanguageLoader.java
@@ -0,0 +1,28 @@
+package org.seekers;
+
+import org.seekers.grpc.SeekersClient;
+
+import java.util.Collection;
+
+/**
+ * Loader for language clients.
+ *
+ * @author karlz
+ * @see SeekersClient
+ */
+public interface LanguageLoader {
+
+    /**
+     * Defines all patterns of file extensions that this loader supports.
+     *
+     * @return the patterns
+     */
+    Collection<String> recognizedPatterns();
+
+    /**
+     * Creates a new seekers client that loads the language file.
+     *
+     * @return a new instance
+     */
+    SeekersClient create();
+}
---
+++ src/main/java/org/seekers/plugin/SeekersExtension.java
@@ -0,0 +1,15 @@
+package org.seekers;
+
+import org.pf4j.ExtensionPoint;
+
+import java.util.List;
+
+public interface SeekersExtension extends ExtensionPoint {
+
+    /**
+     * Extension point to add your own custom language loaders.
+     *
+     * @param loaders list of language loaders
+     */
+    void addLanguageLoaders(final List<LanguageLoader> loaders);
+}
